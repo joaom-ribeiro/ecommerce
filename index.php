db6e4f210dcd1fdcbbc41a4ebd28f92a65ea66fe
João M. Ribeiro <joaomribeiro@paginanaweb.com.br>
2019-02-11 14:37:00 -0200

    adição de classes SQL

--- index.php
+++ index.php
@@ -3,8 +3,12 @@ require_once "vendor/autoload.php";
 
 $app = new Slim\Slim();
 
+$app->config('debug', true);
+
 $app->get('/', function(){
-	echo "Ok";
+	$sql = new SPNW\DB\Sql();	#lembrar de alterar o namespace no arquivo Sql.php
+	$results = $sql->select("SELECT * FROM tb_users");
+	echo json_encode($results);
 });
 
 $app->run();
